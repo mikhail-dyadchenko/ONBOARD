c100bea80aef3e0f7a3ef7837d92a316b38d00db
Mikhail Dyadchenko <mikhail_dyadchenko@mail.ru>
2024-12-08 11:16:06 +0300

    RegistrationFormAdded

--- Gemfile
+++ Gemfile
@@ -36,6 +36,10 @@ gem "bootsnap", require: false
 
 gem "carrierwave", "~> 3.0"
 
+gem "devise"
+
+gem "cancancan"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
@@ -45,6 +49,8 @@ group :development, :test do
 
   # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
   gem "rubocop-rails-omakase", require: false
+
+  gem "letter_opener"
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,51 +8,18 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-@raw_text = 'Дом Наркомфина — один из знаковых памятников архитектуры советского авангарда и конструктивизма. Построен в 1928—1930 годах по проекту архитекторов Моисея Гинзбурга, Игнатия Милиниса и инженера Сергея Прохорова для работников Народного комиссариата финансов СССР (Наркомфина). Автор замысла дома Наркомфина Гинзбург определял его как «опытный дом переходного типа». Дом находится в Москве по адресу: Новинский бульвар, дом 25, корпус 1. С начала 1990-х годов дом находился в аварийном состоянии, был трижды включён в список «100 главных зданий мира, которым грозит уничтожение». В 2017—2020 годах отреставрирован по проекту АБ «Гинзбург Архитектс», функционирует как элитный жилой дом. Отдельно стоящий «Коммунальный блок» (историческое название) планируется как место проведения публичных мероприятий.'
-@words = @raw_text.downcase.gsub(/[—.—,«»:()]/, '').gsub(/  /, ' ').split(' ')
+def create_users
+  i = 0
 
-def seed
-  reset_db
-  create_posts(10)
-  create_comments(2..8)
-end
+  10.times do
+    user_data = {
+      email: "user_#{i}@email.com",
+      password: 'testtest'
+    }
 
-def reset_db
-  Rake::Task['db:drop'].invoke
-  Rake::Task['db:create'].invoke
-  Rake::Task['db:migrate'].invoke
-end
+    user = User.create!(user_data)
+    puts "User created with id #{user.id}"
 
-def create_sentence
-  sentence_words = []
-
-  (10..20).to_a.sample.times do
-    sentence_words << @words.sample
-  end
-
-  sentence = sentence_words.join(' ').capitalize + '.'
-end
-
-def upload_random_image
-  uploader = ImgUploader.new(Post.new, :post_image)
-  uploader.cache!(File.open(Dir.glob(File.join(Rails.root, 'public/autoupload/posts', '*')).sample))
-  uploader
-end
-
-def create_posts(quantity)
-  quantity.times do
-    post = Post.create(title: create_sentence, body: create_sentence)
-    puts "Post with id #{post.id} just created"
+    i += 1
   end
 end
-
-def create_comments(quantity)
-  Post.all.each do |post|
-    quantity.to_a.sample.times do
-      comment = Comment.create(post_id: post.id, body: create_sentence)
-      puts "Comment with id #{comment.id} for post with id #{comment.post.id} just created"
-    end
-  end
-end
-
-seed
